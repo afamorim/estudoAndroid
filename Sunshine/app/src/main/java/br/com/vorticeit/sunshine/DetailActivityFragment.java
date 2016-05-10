94514a4d7fa873391e3be6d81f367b5d8101566d
Antonio Amorim <afamorim@indracompany.com>
2016-05-10 14:57:20 -0300

    Introdução do compartilhar

--- Sunshine/app/src/main/java/br/com/vorticeit/sunshine/DetailActivityFragment.java
+++ Sunshine/app/src/main/java/br/com/vorticeit/sunshine/DetailActivityFragment.java
@@ -4,9 +4,15 @@ import android.app.Fragment;
 import android.content.Intent;
 //import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v7.widget.ShareActionProvider;
 import android.widget.TextView;
 
 import static android.content.Intent.getIntent;
@@ -16,7 +22,13 @@ import static android.content.Intent.getIntent;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = "#SunhineApp";
+    private String mForeCastStr;
+
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -27,11 +39,34 @@ public class DetailActivityFragment extends Fragment {
         View viewRoot = inflater.inflate(R.layout.fragment_detail, container, false);
 
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
-            String foreCast = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mForeCastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
             TextView textView = (TextView)viewRoot.findViewById(R.id.text_forecast);
-            textView.setText(foreCast);
+            textView.setText(mForeCastStr);
         }
-
         return viewRoot;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.detailfragment, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        if (mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }else {
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+    }
+
+    private Intent createShareForecastIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mForeCastStr + FORECAST_SHARE_HASHTAG);
+
+        return shareIntent;
     }
 }
